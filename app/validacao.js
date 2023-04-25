ee8b4414cdf0ea3c397ff259dcff6042236fe601
Saulo Erick <sauloe@hotmail.com>
2023-04-25 11:24:20 -0400

    Pagina de 'Game Over'

--- app/validacao.js
+++ app/validacao.js
@@ -1,12 +1,22 @@
 function verificaChute(){
     const numero = +chute
 
-    if(ChuteForInvalido(numero)){
+    if (chute.toUpperCase()==='GAME OVER'){
+        document.body.classList.add("game-over")
+        document.body.innerHTML =`
+        <h2>GAME OVER</h2>
+        <h3>Que pena, o número secreto era ${numeroSecreto}</h3>
+        <h3>Pressione o botão para tentar novamente</h3>
+        <button id="jogar-novamente" class="btn-game-over">Jogar Novamente</button>
+        `
+    } else if(ChuteForInvalido(numero)){
+        document.getElementById('hidden').classList.add("show")
         elementoChute.innerHTML += '<div>Valor Inválido</div>'
         return
     }
 
     if (chuteInScope(numero)){
+        document.getElementById('hidden').classList.add("show")
         elementoChute.innerHTML += `<div>Valor Inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
         return
     }
@@ -19,10 +29,12 @@ function verificaChute(){
             <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
         `
     } else if (numero > numeroSecreto){
+        document.getElementById('hidden').classList.add("show")
         elementoChute.innerHTML += `
             <div>O número secreto é menor <i class="fa-solid fa-circle-down"></i></div>
         `
     } else {
+        document.getElementById('hidden').classList.add("show")
         elementoChute.innerHTML += `
             <div>O número secreto é maior <i class="fa-solid fa-circle-up"></i></div>
         `
